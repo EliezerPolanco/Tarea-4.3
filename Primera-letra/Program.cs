c8956a8b12cd7b12f77bf6a04a9ac4d079e70e87
EliezerPolanco <eliezerpolanco2427@gmail.com>
2024-08-19 11:14:03 -0400

    Add files via upload

---
+++ Primera-letra/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese su nombre:");
+
+        string nombre = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(nombre) && nombre.All(char.IsLetter))
+        {
+            char inicial = nombre[0];
+
+            Console.WriteLine($"La inicial de tu nombre es: {inicial}");
+        }
+        else
+        {
+            Console.WriteLine("No ingresaste un nombre válido. Asegúrate de ingresar solo letras.");
+        }
+    }
+}
